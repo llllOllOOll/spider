91dcbd571b540745da45dc4d54aa1d8928d6e0f4
seven <7b37b3@gmail.com>
2026-03-13 22:11:02 -0300

    revert: restore std.http.Client implementation

--- src/http_client.zig
+++ src/http_client.zig
@@ -4,33 +4,53 @@ pub const HttpError = error{ RequestFailed, BadStatus };
 
 pub const Header = std.http.Header;
 
+fn readBody(alloc: std.mem.Allocator, response: *std.http.Client.Response, transfer_buffer: []u8, decompress_buffer: []u8) ![]u8 {
+    var decompress: std.http.Decompress = undefined;
+    var body_reader = response.readerDecompressing(transfer_buffer, &decompress, decompress_buffer);
+
+    var body = try std.ArrayList(u8).initCapacity(alloc, 4096);
+    errdefer body.deinit(alloc);
+
+    while (true) {
+        const byte = body_reader.takeByte() catch break;
+        try body.append(alloc, byte);
+    }
+
+    return body.toOwnedSlice(alloc);
+}
+
 pub fn get(
     alloc: std.mem.Allocator,
     io: std.Io,
     url: []const u8,
     headers: []const Header,
 ) ![]u8 {
-    _ = io;
+    var client = std.http.Client{ .allocator = alloc, .io = io };
+    defer client.deinit();
 
-    var args = std.ArrayList([]const u8).init(alloc);
-    defer args.deinit();
+    var req = try client.request(.GET, try std.Uri.parse(url), .{
+        .extra_headers = headers,
+    });
+    defer req.deinit();
 
-    try args.append("curl");
-    try args.append("-s");
-    try args.append(url);
+    try req.sendBodiless();
 
-    for (headers) |h| {
-        try args.append("-H");
-        try args.append(try std.fmt.allocPrint(alloc, "{s}: {s}", .{ h.name, h.value }));
+    var response = try req.receiveHead(&.{});
+
+    if (response.head.status != .ok) {
+        return HttpError.BadStatus;
     }
 
-    var child = std.process.Child.init(args.items, alloc);
-    child.stdout_behavior = .pipe;
+    const decompress_buffer: []u8 = switch (response.head.content_encoding) {
+        .identity => &.{},
+        .zstd => try alloc.alloc(u8, std.compress.zstd.default_window_len),
+        .deflate, .gzip => try alloc.alloc(u8, std.compress.flate.max_window_len),
+        .compress => return HttpError.RequestFailed,
+    };
+    defer if (decompress_buffer.len != 0) alloc.free(decompress_buffer);
 
-    try child.spawn();
-    const stdout = try child.stdout.?.reader().readAllAlloc(alloc, 1024 * 1024);
-    _ = try child.wait();
-    return stdout;
+    var transfer_buffer: [64]u8 = undefined;
+    return readBody(alloc, &response, &transfer_buffer, decompress_buffer);
 }
 
 pub fn post(
@@ -40,26 +60,30 @@ pub fn post(
     body_content: []const u8,
     content_type: []const u8,
 ) ![]u8 {
-    _ = io;
+    var client = std.http.Client{ .allocator = alloc, .io = io };
+    defer client.deinit();
 
-    var args = std.ArrayList([]const u8).init(alloc);
-    defer args.deinit();
+    var req = try client.request(.POST, try std.Uri.parse(url), .{
+        .headers = .{ .content_type = .{ .override = content_type } },
+    });
+    defer req.deinit();
 
-    try args.append("curl");
-    try args.append("-s");
-    try args.append("-X");
-    try args.append("POST");
-    try args.append("-H");
-    try args.append(try std.fmt.allocPrint(alloc, "Content-Type: {s}", .{content_type}));
-    try args.append("-d");
-    try args.append(body_content);
-    try args.append(url);
+    try req.sendBodyComplete(@constCast(body_content));
 
-    var child = std.process.Child.init(args.items, alloc);
-    child.stdout_behavior = .pipe;
+    var response = try req.receiveHead(&.{});
 
-    try child.spawn();
-    const stdout = try child.stdout.?.reader().readAllAlloc(alloc, 1024 * 1024);
-    _ = try child.wait();
-    return stdout;
+    if (response.head.status != .ok) {
+        return HttpError.BadStatus;
+    }
+
+    const decompress_buffer: []u8 = switch (response.head.content_encoding) {
+        .identity => &.{},
+        .zstd => try alloc.alloc(u8, std.compress.zstd.default_window_len),
+        .deflate, .gzip => try alloc.alloc(u8, std.compress.flate.max_window_len),
+        .compress => return HttpError.RequestFailed,
+    };
+    defer if (decompress_buffer.len != 0) alloc.free(decompress_buffer);
+
+    var transfer_buffer: [64]u8 = undefined;
+    return readBody(alloc, &response, &transfer_buffer, decompress_buffer);
 }
